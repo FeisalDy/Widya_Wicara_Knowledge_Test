adc992abf27d38c051291eb57051cec35cc59314
feisaldy <feisaldy26@gmail.com>
2024-12-11 19:48:17 +0700

    setup deploy be to vercel

--- be/src/index.js
+++ be/src/index.js
@@ -8,8 +8,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(router)
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
+export default app
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
